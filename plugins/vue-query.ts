09ae5e9fad6c1614aae2a7f79a9d47ed096504d3
raves21 <johnraves23@gmail.com>
2026-04-25 23:16:42 +0800

    chore: custom queryclient

--- plugins/vue-query.ts
+++ plugins/vue-query.ts
@@ -1,6 +1,22 @@
-import {VueQueryPlugin} from "@tanstack/vue-query";
+import {VueQueryPlugin, QueryClient} from "@tanstack/vue-query";
 
+export const neverRefetchSettings = {
+  gcTime: Infinity,
+  staleTime: Infinity,
+  refetchOnMount: false,
+  refetchOnWindowFocus: false,
+  refetchOnReconnect: false,
+};
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 export default defineNuxtPlugin((app) => {
-    app.vueApp.use(VueQueryPlugin);
+    
+    app.vueApp.use(VueQueryPlugin, {queryClient});
 })
